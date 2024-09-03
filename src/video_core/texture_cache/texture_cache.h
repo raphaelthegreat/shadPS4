c54b68e73e175df20ed8a01dc178790fc78dcc20
IndecisiveTurtle <47210458+raphaelthegreat@users.noreply.github.com>
2024-09-03 14:58:07 +0300

    texture_cache: Use invalidate threshhold
    * It's possible for shaders to bind huge buffers and only write to lower portion of it. This is a problem if upper parts of the buffer overlap with render targets. If the image is very far away from buffer base it's unlikely the shader will want to write it, so skip invalidation for it

--- src/video_core/texture_cache/texture_cache.h
+++ src/video_core/texture_cache/texture_cache.h
@@ -61,11 +61,8 @@ public:
     /// Updates image contents if it was modified by CPU.
     void UpdateImage(ImageId image_id, Vulkan::Scheduler* custom_scheduler = nullptr) {
         Image& image = slot_images[image_id];
-        if (False(image.flags & ImageFlagBits::CpuModified)) {
-            return;
-        }
-        RefreshImage(image, custom_scheduler);
         TrackImage(image, image_id);
+        RefreshImage(image, custom_scheduler);
     }
 
     /// Reuploads image contents.
@@ -124,7 +121,7 @@ private:
         using FuncReturn = typename std::invoke_result<Func, ImageId, Image&>::type;
         static constexpr bool BOOL_BREAK = std::is_same_v<FuncReturn, bool>;
         boost::container::small_vector<ImageId, 32> images;
-        ForEachPage(cpu_addr, size, [this, &images, func](u64 page) {
+        ForEachPage(cpu_addr, size, [this, &images, cpu_addr, size, func](u64 page) {
             const auto it = page_table.find(page);
             if (it == nullptr) {
                 if constexpr (BOOL_BREAK) {
@@ -136,6 +133,9 @@ private:
             for (const ImageId image_id : *it) {
                 Image& image = slot_images[image_id];
                 if (image.flags & ImageFlagBits::Picked) {
+                    continue;
+                }
+                if (!image.Overlaps(cpu_addr, size)) {
                     continue;
                 }
                 image.flags |= ImageFlagBits::Picked;
